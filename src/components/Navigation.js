9927f808082fa7da23f98f584c44b250436eb158
siIvin <garavito.silvia@gmail.com>
2020-05-20 15:06:55 -0600

    componente de barra de herramientas

---
+++ src/components/Navigation.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+import { Link } from 'react-router-dom';
+
+
+
+const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Biblioteca</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink tag = {Link} to="/login">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag = {Link} to = "/signup">signup</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+
+
+export default Navigation;
