5e27a60891f8557bb6fa2ab5ed6ca8aa07d5f37c
siIvin <garavito.silvia@gmail.com>
2020-05-20 18:12:10 -0600

    agregar contexto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 
 //Components
-import {Navigation} from './components'
+import { Navigation, Footer } from './components'
 
 //import {Container} from 'reactstrap'
-import {BrowserRouter as Router, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import Routes from './Routes';
-    
+
+import AuthContextProvider from './context/AuthContext'
 
 
 function App() {
   return (
     <Fragment>
       <Router>
-        <Navigation/>
-        <Switch>
-          {Routes}
-        </Switch>
+        <AuthContextProvider>
+
+          <Navigation />
+          <Switch>
+            {Routes}
+          </Switch>
+          <Footer />
+        </AuthContextProvider>
       </Router>
     </Fragment>
   );
