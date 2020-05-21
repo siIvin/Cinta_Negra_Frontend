de21bd8b34cf1256766116431f1a337116f6def8
siIvin <garavito.silvia@gmail.com>
2020-05-20 18:12:36 -0600

    vista de estudiantes

---
+++ src/views/students.js
@@ -0,0 +1,21 @@
+
+
+import React from 'react';
+
+import { Profile } from '../components'
+
+
+const student = () => {
+
+
+    return (
+        <React.Fragment>
+            <Profile>
+                HOla
+     </Profile>
+
+        </React.Fragment>
+    )
+}
+
+export default student
