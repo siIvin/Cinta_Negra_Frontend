5e2a2069e19b52a841eb6d5a82bcee84e89da5fa
siIvin <garavito.silvia@gmail.com>
2020-05-20 18:10:06 -0600

    card para estudiantes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,10 +10,10 @@ const Profile = (props) => {
       <Card>
         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardTitle>Nombre</CardTitle>
+          <CardSubtitle>Edad</CardSubtitle>
           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
+          <Button>Nueva Lectura</Button>
         </CardBody>
       </Card>
     </div>
