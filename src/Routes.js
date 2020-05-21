40e8ef94c956cce14d64296df0c8d3ae3b7c126d
siIvin <garavito.silvia@gmail.com>
2020-05-21 15:09:49 -0600

    ruta para form de libros

--- src/Routes.js
+++ src/Routes.js
@@ -5,15 +5,18 @@ import Home from './views/home';
 import Login from './views/login';
 import Signup from './views/signup';
 import student from './views/students'
+import BooksForm from './views/bookForm'
 //mport Example from './views/students';
 
 
 export default[
     <React.Fragment>        
+        
         <Route exact path ="/" component = {Home}></Route>
         <Route exact path ="/login" component = {Login}></Route>
         <Route exact path = "/signup" component = {Signup}></Route>
         <Route exact path = "/students" component = {student}></Route>
+        <Route exact path="/books-form" component ={BooksForm}></Route>
 
         
     </React.Fragment>
