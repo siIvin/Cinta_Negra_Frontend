ca88b5b3442d3ccb5e952d6e46943487ba349da1
siIvin <garavito.silvia@gmail.com>
2020-05-21 19:23:21 -0600

    renderizar lista de estudiantes

--- src/views/students.js
+++ src/views/students.js
@@ -1,21 +1,37 @@
+import React, {useState, useContext, useEffect} from 'react';
+import {AuthContext} from '../context/AuthContext';
+import {StudentCard} from '../components'
 
 
-import React from 'react';
+const StudentList = () =>{
+    const {axiosInstance} = useContext(AuthContext);
+    const [students, setStudent] = useState ([])
+    const [text, setText]= useState('Cargando estudiantes')
+    const getStudents = () => axiosInstance.get('/api/v1/students');
 
-import { Profile } from '../components'
+    useEffect (()=>{
+        getStudents()
+        .then((response)=>{
+            const students = response.data;
+            setStudent (students);
+            setText (`Hay ${students.length} inscritos a la biblioteca`)
+        })
+        .catch(()=>setText('No hay estudiantes inscritos'))
+    }, [])
 
-
-const student = () => {
-
-
-    return (
+    return(
         <React.Fragment>
-            <Profile>
-                HOla
-     </Profile>
-
+            {students.map((student)=>{
+                return(  
+                <StudentCard
+                key ={student._id}
+                first_name= {student.first_name}
+                last_name ={student.last_name}
+                dateOfBirth = {student.dateOfBirth}/>
+            )})}
         </React.Fragment>
-    )
+    );
+
 }
 
-export default student
+export default StudentList
