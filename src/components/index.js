0739405b6145897a3b8dd2d5b8ad905804a0134a
siIvin <garavito.silvia@gmail.com>
2020-05-21 19:24:02 -0600

    componente card de estudiantes

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 import Navigation from './Navigation';
-import Profile from './Profile';
+import StudentCard from './StudentCard';
 import Footer from './Footer';
 
 
-export {Navigation, Profile, Footer}
+export {Navigation, StudentCard, Footer}
 
 
 
