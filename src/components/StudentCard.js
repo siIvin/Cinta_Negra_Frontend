aee54dc2de378324fd9eca6e7c90e6f5c8d8e500
siIvin <garavito.silvia@gmail.com>
2020-05-22 11:07:22 -0600

    edad de estudiantes

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -5,14 +5,24 @@ import {
 } from 'reactstrap';
 
 const StudentCard = (props) => {
+  
+  const studentAge=(dateOfBirth)=>{
+    console.log(props)
+    const dateNow = Date.now()
+    const age = dateNow - new Date(dateOfBirth)
+    return Math.floor(age/31536000000)
+  }
+
+  
+
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardImg top width="100%" src={props.image_profile} alt="Card image cap" />
         <CardBody>
           <CardTitle>Nombre: {props.first_name}</CardTitle>
           <CardSubtitle>Apellido: {props.last_name}</CardSubtitle>
-          <CardText>Edad: {props.dateOfBirth}</CardText>
+          <CardText>Edad: {studentAge(props.dateOfBirth)} años</CardText>
           <Button>Nueva Lectura</Button>
         </CardBody>
       </Card>
